b9dae8d6352dbebea325078a0c6bae72cc1d40a2
raphael-caninde <raphael.caninde@gmail.com>
2022-07-05 13:51:17 -0300

    add dockerfile e node script

---
+++ backend/app/src/app.ts
@@ -0,0 +1,36 @@
+import * as express from 'express';
+import * as cors from 'cors';
+
+class App {
+
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  private routes():void {
+
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => { console.log(`Rodando na porta ${PORT}`); });
+  }
+}
+
+export { App };
--- backend/app/src/index.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-require('dotenv').config()
-
-app.listen(PORT, () => console.log(`Online on port ${ process.env.PORT }`));
