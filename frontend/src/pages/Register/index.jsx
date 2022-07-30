7899876fbb65b8edf33be4b8a3e216fe2bec58b6
raphael-caninde <raphael.caninde@gmail.com>
2022-07-30 18:03:34 -0300

    feat: adiciona formulario de cadastro

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../../context/AppContext';
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
-}
+  const { infoRegister: { name, lastName, email, password }, handleChange } = useContext(AppContext);
 
-export default Register
+  return (
+    <div>
+      <h3>Faça seu cadastro</h3>
+      <form>
+        <div>
+          <label htmlFor="name">Nome</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ handleChange }
+            />
+          <label htmlFor="lastName">sobrenome</label>
+          <input
+            id="lastName"
+            type="text"
+            name="lastName"
+            value={ lastName }
+            onChange={ handleChange }
+            />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ handleChange }
+          />
+          <label htmlFor="password">Senha</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ handleChange }
+            />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
