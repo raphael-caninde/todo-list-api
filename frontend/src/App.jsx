67e312bc8f0ec36bc12d59d686abab528d854fc5
raphael-caninde <raphael.caninde@gmail.com>
2023-02-04 00:02:32 -0300

    feat: adiciona  react query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import { Router } from './routes/Routes';
 import AppProvider from './context/AppProvider';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 function App() {
   return (
-    <AppProvider>
-      <Router />
-    </AppProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <Router />
+      </AppProvider>
+    </QueryClientProvider>
   );
 }
 
