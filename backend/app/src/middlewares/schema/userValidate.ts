f49a60838da3f6bc0337fec3866f75578e4aec50
raphael-caninde <raphael.caninde@gmail.com>
2023-02-09 23:48:52 -0300

    feat: adiciona mensagens customizadas nas validações dos campos

--- backend/app/src/middlewares/schema/userValidate.ts
+++ backend/app/src/middlewares/schema/userValidate.ts
@@ -1,18 +1,33 @@
 import Joi from "joi";
 
-const userValidation = Joi.object().keys({
-  name: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).max(16).required(),
+export const userValidation = Joi.object().keys({
+  name: Joi.string().min(3).required().messages({
+    'string.empty': 'O campo "nome" não pode ser vazio.',
+    'string.min': 'O campo tem que ter no mínimo 3 caracteres.',
+  }),
+  lastName: Joi.string().min(3).required().messages({
+    'string.empty': 'O campo "sobrenome" não pode ser vazio.',
+    'string.min': 'O campo tem que ter no mínimo 3 caracteres.',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.empty': 'O campo email não pode ser vazio.',
+    'string.email': '"email" deve ser um e-mail válido.',
+  }),
+  password: Joi.string().min(6).max(16).required().messages({
+    'string.empty': 'O campo "senha" não pode ser vazio.',
+    'string.min': 'A senha tem que ter no mínimo 6 caracteres.',
+    'string.max': 'A senha tem que ter no máximo 16 caracteres.',
+  }),
 });
 
-const login = Joi.object().keys({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).max(16).required(),
+export const login = Joi.object().keys({
+  email: Joi.string().email().required().messages({
+    'string.empty': 'O campo email não pode ser vazio.',
+    'string.email': '"email" deve ser um e-mail válido.',
+  }),
+  password: Joi.string().min(6).max(16).required().messages({
+    'string.empty': 'O campo "senha" não pode ser vazio.',
+    'string.min': 'E-mail ou senha incorretos.',
+    'string.max': 'E-mail ou senha incorretos.',
+  }),
 });
-
-export {
-  userValidation,
-  login,
-};
