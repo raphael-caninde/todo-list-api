5b1570b26c9f93a2d201f87a8e492a6b67f3f8d8
raphael-caninde <raphael.caninde@gmail.com>
2023-02-09 23:47:38 -0300

    refactor: mudança no nome do arquivo

--- backend/app/src/middlewares/validateJoi.ts
+++ backend/app/src/middlewares/validateJoi.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 
 const validInputs = (schema: Joi.Schema) => (req:Request, res:Response, next:NextFunction) => {
   const { error } = schema.validate(req.body);
-  
+
   if (Joi.isError(error)) {
     return res.status(401).json({ message: error.details[0].message });
   };
