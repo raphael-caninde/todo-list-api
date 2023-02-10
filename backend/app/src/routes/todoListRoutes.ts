043f8d13d80c8ea9f013720266ed6b141de1f61c
raphael-caninde <raphael.caninde@gmail.com>
2023-02-09 23:49:21 -0300

    feat: implementa função de validação de campos

--- backend/app/src/routes/todoListRoutes.ts
+++ backend/app/src/routes/todoListRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import TodoListController from '../controllers/todoListController';
-import checkToken from '../middlewares/validateToken'
+import checkToken from '../middlewares/validateToken';
+import validInputs from '../middlewares/validateJoi';
+import { inputTask } from '../middlewares/schema/taskValidate';
 
 const router = Router();
 const taskConttroller = new TodoListController();
 
 router.get('/', checkToken, taskConttroller.getList);
 
-router.post('/', checkToken, taskConttroller.createTask);
+router.post('/', checkToken, validInputs(inputTask), taskConttroller.createTask);
 
-router.patch('/:id',checkToken, taskConttroller.updateTask);
+router.patch('/:id', checkToken, validInputs(inputTask), taskConttroller.updateTask);
 
 router.delete('/:id', checkToken, taskConttroller.deleteTask);
 
