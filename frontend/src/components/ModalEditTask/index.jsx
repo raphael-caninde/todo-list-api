36167424e4f5725b1e3676e00202361db761629d
raphael-caninde <raphael.caninde@gmail.com>
2023-02-23 22:21:04 -0300

    style: customização modal de editar

--- frontend/src/components/ModalEditTask/index.jsx
+++ frontend/src/components/ModalEditTask/index.jsx
@@ -54,38 +54,46 @@ export function ModalEditTask({ openModal: { open, id, task }, setIsOpenModal })
 
   return (
     <ReactModal
+      className='flex w-80 p-3 bg-neutral-700 rounded-lg absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3'
       isOpen={ open }
       onRequestClose={ handleCloseModal }
       ariaHideApp={ false }
     >
       <AiOutlineCloseSquare
+        className="absolute top-2 right-2 text-white bg-red-500 w-7 h-7 rounded-sm cursor-pointer"
         onClick={handleCloseModal}
       />
-
-      <div className="">
-        <div>
+      <div className="flex flex-col items-center w-full mt-5">
+        <div className='text-lg text-white mb-4'>
           <h3>{ task }</h3>
         </div>
         <input
+          className='w-full outline-none text-lg p-1 rounded-md'
           type="text"
           value={ inputText }
           name='textInput'
           onChange={({ target }) => setInputText(target.value)}
           onKeyUp={handleKeyUp}
         />
-        <span>{error}</span>
-        <button
-          type='button'
-          onClick={() => editTask({ id, inputText })}
-        >
-          ADICIONAR
-        </button>
-        <button
-          type='button'
-          onClick={() => handleCloseModal()}
-        >
-          CANCELAR
-        </button>
+        <span className='text-red-600 font-medium'>
+          {error}
+        </span>
+        <div className='flex gap-4 text-white mt-4'>
+          <button
+            className='px-5 py-1 cursor-pointer rounded-lg bg-red-500'
+            type='button'
+            onClick={() => handleCloseModal()}
+          >
+            Cancelar
+          </button>
+          <button
+            className='px-5 py-1 cursor-pointer rounded-lg bg-blue-500'
+            type='button'
+            onClick={() => editTask({ id, inputText })}
+          >
+            Adicionar
+          </button>
+        </div>
       </div>
     </ReactModal>
   )
