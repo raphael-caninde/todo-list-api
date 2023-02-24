8837d382182c632727ae7fd2a2d725607998e389
raphael-caninde <raphael.caninde@gmail.com>
2023-02-23 22:20:19 -0300

    style: customização modal de deletar

--- frontend/src/components/ModalDeleteTask/index.jsx
+++ frontend/src/components/ModalDeleteTask/index.jsx
@@ -1,4 +1,4 @@
-import ReactModal from "react-modal";
+import Modal from "react-modal";
 import { AiOutlineCloseSquare } from "react-icons/ai";
 import { useMutation, useQueryClient } from "react-query";
 import { toast } from "react-toastify";
@@ -37,33 +37,38 @@ export function ModalDeleteTask({openModalDelete: {open, id}, setIsOpenModalDele
 );
 
   return (
-    <ReactModal
+    <Modal
+      className='flex w-80 p-3 bg-neutral-700 rounded-lg absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3'
       isOpen={ open }
       onRequestClose={ closeModal }
       ariaHideApp={ false }
     >
-      <div>
+      <div className="flex flex-col w-full relative">
         <AiOutlineCloseSquare
+          className="absolute top-0 right-0 text-white bg-red-500 w-7 h-7 rounded-sm cursor-pointer"
+          size={23}
           onClick={() => closeModal()}
         />
-        <div>
+        <div className="w-full text-lg text-center mt-9 text-white">
           <span>Deseja realmente excluir a tarefa?</span>
         </div>
-        <div>
+        <div className="flex justify-center w-full gap-4 my-4">
           <button
-            type="submit"
-            onClick={() => deleteTask(id)}
-          >
-            Sim
-          </button>
-          <button
+            className="px-5 py-1 cursor-pointer rounded-lg bg-red-500 text-white"
             type="button"
             onClick={() => closeModal()}
           >
             Não
           </button>
+          <button
+            className="px-5 py-1 cursor-pointer rounded-lg bg-blue-500 text-white"
+            type="submit"
+            onClick={() => deleteTask(id)}
+          >
+            Sim
+          </button>
         </div>
       </div>
-    </ReactModal>
+    </Modal>
   )
 }
